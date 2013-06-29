d4d4b9c8d4946b9c1a999b5663b33399052a2f98
Bryan Kerr <bkerr1080@gmail.com>
2013-06-28 21:09:51 -0600

    New Project

---
+++ Ultimate Apartment Guide/2coltest.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ultimate_Apartment_Guide
+{
+    public partial class _2coltest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
